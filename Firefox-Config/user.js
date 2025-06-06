0de4b7561513adb2eddd336fbd2638add075c453
Kshitij <notkshitij@git.kska.io>
2025-06-06 13:06:11 +0530

    Restored to default betterfox user.js

--- Firefox-Config/user.js
+++ Firefox-Config/user.js
@@ -12,11 +12,11 @@
  * "Ad meliora"                                                             *
  * version: 138                                                             *
  * url: https://github.com/yokoffing/Betterfox                              *
- ****************************************************************************/
+****************************************************************************/
 
 /****************************************************************************
  * SECTION: FASTFOX                                                         *
- ****************************************************************************/
+****************************************************************************/
 /** GENERAL ***/
 user_pref("content.notify.interval", 100000);
 
@@ -55,14 +55,13 @@ user_pref("browser.places.speculativeConnect.enabled", false);
 user_pref("network.prefetch-next", false);
 user_pref("network.predictor.enabled", false);
 user_pref("network.predictor.enable-prefetch", false);
-user_pref("browser.send_pings", false); // [DEFAULT: false]
 
 /** EXPERIMENTAL ***/
 user_pref("layout.css.grid-template-masonry-value.enabled", true);
 
 /****************************************************************************
  * SECTION: SECUREFOX                                                       *
- ****************************************************************************/
+****************************************************************************/
 /** TRACKING PROTECTION ***/
 user_pref("browser.contentblocking.category", "strict");
 user_pref("browser.download.start_downloads_in_tmp_dir", true);
@@ -97,12 +96,8 @@ user_pref("browser.urlbar.quicksuggest.enabled", false);
 user_pref("browser.urlbar.groupLabels.enabled", false);
 user_pref("browser.formfill.enable", false);
 user_pref("network.IDN_show_punycode", true);
-user_pref("browser.urlbar.suggest.quicksuggest.nonsponsored", false); // [FF95+]
-user_pref("browser.urlbar.suggest.quicksuggest.sponsored", false); // [FF92+]
-user_pref("browser.urlbar.suggest.searches", false);
 
 /** PASSWORDS ***/
-user_pref("signon.autofillForms", false); 
 user_pref("signon.formlessCapture.enabled", false);
 user_pref("signon.privateBrowsingCapture.enabled", false);
 user_pref("network.auth.subresource-http-auth-allow", 1);
@@ -123,12 +118,6 @@ user_pref("privacy.userContext.ui.enabled", true);
 
 /** SAFE BROWSING ***/
 user_pref("browser.safebrowsing.downloads.remote.enabled", false);
-user_pref("browser.safebrowsing.malware.enabled", false);
-user_pref("browser.safebrowsing.phishing.enabled", false);
-user_pref("browser.safebrowsing.downloads.enabled", false);
-user_pref("browser.safebrowsing.downloads.remote.url", ""); // Defense-in-depth
-user_pref("browser.safebrowsing.downloads.remote.block_potentially_unwanted", false);
-user_pref("browser.safebrowsing.downloads.remote.block_uncommon", false);
 
 /** MOZILLA ***/
 user_pref("permissions.default.desktop-notification", 2);
@@ -165,26 +154,18 @@ user_pref("app.normandy.api_url", "");
 /** CRASH REPORTS ***/
 user_pref("breakpad.reportURL", "");
 user_pref("browser.tabs.crashReporting.sendReport", false);
-user_pref("browser.crashReports.unsubmittedCheck.autoSubmit2", false); // [DEFAULT: false]
 
 /****************************************************************************
  * SECTION: PESKYFOX                                                        *
- ****************************************************************************/
+****************************************************************************/
 /** MOZILLA UI ***/
 user_pref("browser.privatebrowsing.vpnpromourl", "");
 user_pref("extensions.getAddons.showPane", false);
 user_pref("extensions.htmlaboutaddons.recommendations.enabled", false);
 user_pref("browser.discovery.enabled", false);
 user_pref("browser.shell.checkDefaultBrowser", false);
-user_pref("browser.shopping.experience2023.enabled", false); // [DEFAULT: false]
-user_pref(
-  "browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons",
-  false,
-);
-user_pref(
-  "browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features",
-  false,
-);
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons", false);
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features", false);
 user_pref("browser.preferences.moreFromMozilla", false);
 user_pref("browser.aboutConfig.showWarning", false);
 user_pref("browser.aboutwelcome.enabled", false);
@@ -204,13 +185,6 @@ user_pref("full-screen-api.warning.timeout", 0);
 user_pref("browser.urlbar.unitConversion.enabled", true);
 user_pref("browser.urlbar.trending.featureGate", false);
 user_pref("dom.text_fragments.create_text_fragment.enabled", true);
-user_pref("browser.urlbar.addons.featureGate", false); // [FF115+]
-user_pref("browser.urlbar.fakespot.featureGate", false); // [FF130+] [DEFAULT: false]
-user_pref("browser.urlbar.mdn.featureGate", false); // [FF117+] [HIDDEN PREF]
-user_pref("browser.urlbar.pocket.featureGate", false); // [FF116+] [DEFAULT: false]
-user_pref("browser.urlbar.weather.featureGate", false); // [FF108+] [DEFAULT: false]
-user_pref("browser.urlbar.yelp.featureGate", false); // [FF124+]
-user_pref("browser.urlbar.clipboard.featureGate", false);
 
 /** NEW TAB PAGE ***/
 user_pref("browser.newtabpage.activity-stream.default.sites", "");
@@ -235,102 +209,21 @@ user_pref("layout.word_select.eat_space_to_next_word", false);
 
 /****************************************************************************
  * START: MY OVERRIDES                                                      *
- ****************************************************************************/
+****************************************************************************/
 // visit https://github.com/yokoffing/Betterfox/wiki/Common-Overrides
 // visit https://github.com/yokoffing/Betterfox/wiki/Optional-Hardening
 // Enter your personal overrides below this line:
 
-// PREF: restore login manager
-user_pref("signon.rememberSignons", true);
-
-// PREF: restore Top Sites on New Tab page
-user_pref("browser.newtabpage.activity-stream.feeds.topsites", true);
-
-// PREF: enable container tabs
-user_pref("privacy.userContext.enabled", true);
-
-/****************************************************************************
- * HARDENING                                                                *
- ***************************************************************************/
-
-// PREF: disable Firefox Sync
-user_pref("identity.fxaccounts.enabled", false);
-
-// PREF: disable the Firefox View tour from popping up
-user_pref("browser.firefox-view.feature-tour", '{"screen":"","complete":true}');
-
-// PREF: enable HTTPS-Only Mode
-// Warn me before loading sites that don't support HTTPS
-// in both Normal and Private Browsing windows.
-user_pref("dom.security.https_only_mode", true);
-user_pref("dom.security.https_only_mode_error_page_user_suggestions", true);
-
-// PREF: disable captive portal detection
-// [WARNING] Do NOT use for mobile devices!
-user_pref("captivedetect.canonicalURL", "");
-user_pref("network.captive-portal-service.enabled", false);
-user_pref("network.connectivity-service.enabled", false);
-
-// PREF: set DoH provider
-user_pref("network.trr.uri", "https://family.dns.mullvad.net/dns-query"); // Hagezi Light + TIF
-
-// PREF: enforce DNS-over-HTTPS (DoH)
-user_pref("network.trr.mode", 3);
-
-// PREF: enforce certificate pinning
-// [ERROR] MOZILLA_PKIX_ERROR_KEY_PINNING_FAILURE
-// 1 = allow user MiTM (such as your antivirus) (default)
-// 2 = strict
-user_pref("security.cert_pinning.enforcement_level", 2);
-
-// PREF: delete all browsing data on shutdown
-user_pref("privacy.sanitize.sanitizeOnShutdown", true);
-user_pref("privacy.clearOnShutdown_v2.cache", true);
-
-// PREF: after crashes or restarts, do not save extra session data
-// such as form content, scrollbar positions, and POST data
-user_pref("browser.sessionstore.privacy_level", 2);
-
-// PREF: disable all DRM content
-user_pref("media.eme.enabled", false);
-
-// [SETTING] General>Language>Choose your preferred language for displaying pages>Choose>Request English...
-user_pref("privacy.spoof_english", 1);
-
-
-/****************************************************************************
- * BEGINNING OF ARKEN FOX                                                   *
- ***************************************************************************/
-
-// Geo-location
-user_pref("geo.provider.use_geoclue", false); // [FF102+] [LINUX]
-
-/****************************************************************************
- * END OF ARKEN FOX                                                         *
- ***************************************************************************/
 
 
 /****************************************************************************
  * SECTION: SMOOTHFOX                                                       *
- ****************************************************************************/
+****************************************************************************/
 // visit https://github.com/yokoffing/Betterfox/blob/main/Smoothfox.js
 // Enter your scrolling overrides below this line:
 
-/****************************************************************************************
- * OPTION: SHARPEN SCROLLING                                                           *
- ****************************************************************************************/
-// credit: https://github.com/black7375/Firefox-UI-Fix
-// only sharpen scrolling
-user_pref("apz.overscroll.enabled", true); // DEFAULT NON-LINUX
-user_pref("general.smoothScroll", true); // DEFAULT
-user_pref("mousewheel.min_line_scroll_amount", 10); // adjust this number to your liking; default=5
-user_pref("general.smoothScroll.mouseWheel.durationMinMS", 80); // default=50
-user_pref("general.smoothScroll.currentVelocityWeighting", "0.15"); // default=.25
-user_pref("general.smoothScroll.stopDecelerationWeighting", "0.6"); // default=.4
-// Firefox Nightly only:
-// [1] https://bugzilla.mozilla.org/show_bug.cgi?id=1846935
-user_pref("general.smoothScroll.msdPhysics.enabled", false); // [FF122+ Nightly]
+
 
 /****************************************************************************
  * END: BETTERFOX                                                           *
- ****************************************************************************/
+****************************************************************************/
